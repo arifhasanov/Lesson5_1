39edabc9d858ecfd79cd89f95a981638bbe22b3d
Arif Hasanov <arifhasanov@yahoo.com>
2021-11-16 09:56:07 +0100

    Done

---
+++ Lesson5_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "text.txt";
+            string content = Console.ReadLine();
+            File.WriteAllText(filename, content);
+
+        }
+    }
+}
